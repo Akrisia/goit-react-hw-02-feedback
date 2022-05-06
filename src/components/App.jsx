0c0477ca324ea55d4d1ba53a4d0e20e3cbccbd09
Iryna S <irynaradkovska@gmail.com>
2022-05-07 02:13:04 +0300

    adds 1st step of the 1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Feedback } from './Feedback';
+
+function App() {
+    return (
+      <div>
+        <Feedback />
+      </div>
+    );
+  }
+
+export default App;
